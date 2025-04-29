4725a70e601192aa01818129f3c5170e8c4a819b
Julian Sabo <6301297@edu.vaniercollege.qc.ca>
2025-04-29 15:35:59 -0400

    Modify, overLoading for the bubble sort method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,22 @@ public class Main {
     public static void main(String[] args) {
 
         }
-    public static void bubbleSort(int[] nums) {
+        public static void bubbleSort(int[] nums) {
+        bubbleSort(nums, true);
+        }
+
+    public static void bubbleSort(int[] nums, boolean ascending) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length -1 - i; j++) {
                 if (nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j +1] = temp;
+                    boolean condition = ascending
+                     ? nums[j] > nums[j + 1]
+                     : nums[j] < nums[j + 1];
+                     if (condition) {
+                         int temp = nums[j];
+                         nums[j] = nums[j + 1];
+                         nums[j + 1] = temp;
+                     }
                 }
             }
         }
